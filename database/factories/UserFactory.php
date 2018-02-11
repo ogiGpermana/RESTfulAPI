d51f80d19cc1d5f4e3a7a711a37767058a308560
Ogi G Permana <hunterdumay@gmail.com>
2018-02-11 14:11:25 +0700

    Creating the factory for Product

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Category;
+use App\Product;
 use App\User;
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,7 @@ use App\User;
 
 $factory->define(User::class, function (Faker\Generator $faker) {
 	static $password;
-	
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
@@ -30,6 +31,17 @@ $factory->define(User::class, function (Faker\Generator $faker) {
 $factory->define(Category::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
-        'email' => $faker->paragraph(1),
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        'seller_id' => User::all()->random()->id, // User::inRandomOrder()->first()->id 
     ];
 });
