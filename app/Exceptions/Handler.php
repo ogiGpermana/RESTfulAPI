7b962d22cf7261be5e48db378dc9cf5e1417214e
Ogi G Permana <hunterdumay@gmail.com>
2018-02-15 11:30:37 +0700

    Handling Unauthentication Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -63,7 +63,23 @@ class Handler extends ExceptionHandler
             return $this->errorResponse("Does not exists any {$modelName} with the specified indentificator", 404);
         }
 
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($exception, $request);
+        }
+
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('Unauthenticated.', 401);
     }
 
     /**
